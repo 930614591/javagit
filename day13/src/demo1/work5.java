a146e88ba74bf70406d802a0da99240391fa18f6
930614591 <930614591@qq.com>
2021-09-03 00:04:13 +0800

    作业6

---
+++ day13/src/demo1/work5.java
@@ -0,0 +1,13 @@
+package demo1;
+
+import java.util.Scanner;
+
+public class work5 {
+    //6、控制台输入金额，转换成字符串输出
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入银行卡金额：");
+    }
+
+
+}
