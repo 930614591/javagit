80379da0ea10342f25346906d49205f8d79c1f7c
930614591 <930614591@qq.com>
2021-09-05 14:08:12 +0800

    arraylist

--- day13/src/listDemo/Demo1.java
+++ day13/src/listDemo/Demo1.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class Demo1 {
     public static void main(String[] args) {
         //1.jkd1.5版本时候的集合
-        ArrayList list=new ArrayList();
+        ArrayList  list=new ArrayList();
         list.add(1);
         list.add(2);
         list.add(3);
-        list.add(4);
+        list.add("1");
        // System.out.println(list.toString());
         //1. 查看集合中的元素
         System.out.println(list);
@@ -17,6 +17,15 @@ public class Demo1 {
 //        list.get()
         //2.获取指定索引处元素的集合list.get(index)
         Object obj=list.get(3);
-        System.out.println(obj);
+        if (obj instanceof  Integer){
+            int num=(Integer)obj;
+            System.out.println(num*100);
+        }
+        Integer n=(Integer) list.get(1);//类型转换异常 下转型
+        //工作中：集合推荐大家使用同一种数据类型
+        //为了解决下转型带来的安全问题，在1.5版本提出了全新的概念，泛型：
+        //泛型：表示广泛的类型（通用类型的规范）
+        //语法： <任意类型><t=T><E><K,V>当自己定义泛型时建议用大写
+
     }
 }
---
+++ day13/src/listDemo/demotow.java
@@ -0,0 +1,25 @@
+package listDemo;
+
+import java.util.ArrayList;
+/*
+* 泛型的作用
+*   将类型的检查1从运行期提前到了编译期：有效地解决了下转型的安全隐患
+* */
+public class demotow {
+    public static void main(String[] args) {
+        //1.jdk1.5之后解决了下转型的安全隐患
+        ArrayList<Integer> list=new ArrayList<Integer>();//当前这个集合只能存放int类型数据
+        //1.像集合中追加元素
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2,999);
+        //list.add("1");
+        //2. 查看集合元素个数
+        System.out.println(list.size());
+        //。3、像集指定位置合中覆盖元素
+        list.set(1,998);
+        System.out.println(list);
+
+    }
+}
