a900c925117f4387a27b414a4ad99991bef5ada8
930614591 <930614591@qq.com>
2021-09-05 20:35:38 +0800

    泛型类

--- Day14/src/Moder.java
+++ Day14/src/Moder.java
@@ -1,7 +1,11 @@
 package array;
 
 public class Moder<T> {
+    private T obj;
     public void test(T obj){
         System.out.println(obj);
+    }
+    public T returnObj(){
+        return obj;
     }
 }
