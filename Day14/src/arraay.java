658d5134a1e943ebb4cefb855b22c659ad848978
930614591 <930614591@qq.com>
2021-09-12 09:07:18 +0800

    开学第一天--数组

---
+++ Day14/src/arraay.java
@@ -0,0 +1,15 @@
+package array;
+
+public class arraay {
+    //一维数组
+    int [] arr=new int[10];
+    int [] arr1=new int[] {1,2,3,4};
+    int [] arr2={1,2,3,4};
+    //二维数组
+    int [][] arrr=new int[10][10];
+    int [][] arrr1=new int[10][];
+    int[][] arrr2={{1,2,3},{1,2,3},{1,2,3}};
+    //数组元素地址是连续的
+//    索引默认从0开始
+//    最大索引=长度-1
+}
