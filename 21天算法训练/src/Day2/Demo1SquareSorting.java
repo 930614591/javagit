7f104b0279ff07b6819ed2f19de1042598160331
930614591 <930614591@qq.com>
2021-09-16 07:38:13 +0800

    给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。

--- 21天算法训练/src/Day2/Demo1SquareSorting.java
+++ 21天算法训练/src/Day2/Demo1SquareSorting.java
@@ -1,5 +1,5 @@
 package Day2;
-
+//给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
 public class Demo1SquareSorting {
     public static void main(String[] args) {
         int [] z={-4,-1,0,3,10};
@@ -7,7 +7,7 @@ public class Demo1SquareSorting {
     }
     public static int[] sortedSquares(int[] nums) {
         int n=nums.length;
-        int [] temp2=new int[n];
+       // int [] temp2=new int[n];
         for(int i=0;i<n;i++){
             nums[i]*=nums[i];
             /*for(int j=0;j<n-1;j++){
@@ -27,6 +27,6 @@ public class Demo1SquareSorting {
                 }
             }
         }
-        return temp2;
+        return nums;
     }
 }
