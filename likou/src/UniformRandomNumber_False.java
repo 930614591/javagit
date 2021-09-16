794c4897b31ad81accf3dedfc5420f6b744b56ae
930614591 <930614591@qq.com>
2021-09-16 22:41:29 +0800

    欧皇测试器，真正均匀生成随机数的想法，由0开始计算，过程耗时较长

--- likou/src/UniformRandomNumber_False.java
+++ likou/src/UniformRandomNumber_False.java
@@ -9,14 +9,23 @@ import java.util.Random;
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
 public class UniformRandomNumber_False {
-    public int rand8() {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            int o=rand10();
+            if(o==10){
+                System.out.println("宁难道就是欧皇吗？居然抽到了10！");
+            }else
+            System.out.println(o+"加油，下次一定！");
+        }
+    }
+    public static int rand8() {
         return (rand4()-1)*2+rand2();//rand4*2
     }
-    private  int rand16(){
-        return  (rand8()-1*2)+rand2();
+    private static int rand16(){
+        return  (rand8()-1)*2+rand2();
     }
     //由rand16做到rand10
-    private  int rand10(){
+    private static int rand10(){
         while (true) {
             int num = rand16();
             if(num<=10){
@@ -26,11 +35,11 @@ public class UniformRandomNumber_False {
     }
 
 
-    private  int rand2(){//均匀生成1，2
+    private static int rand2(){//均匀生成1，2
         Random df=new Random();
         return df.nextInt(2)+1;
     }
-    private int rand4(){//均匀生成1到4
+    private static int rand4(){//均匀生成1到4
         return (rand2()-1)*2+rand2();
     }
 }
--- likou/src/WordSearchII.java
+++ likou/src/WordSearchII.java
@@ -1,11 +1,14 @@
+/*
 import java.util.List;
+*/
 /*给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words，找出所有同时在二维网格和字典中出现的单词。
 
 单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
 
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/word-search-ii
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*//*
+
 public class WordSearchII {
     public List<String> findWords(char[][] board, String[] words) {
         int temp1=board.length;
@@ -34,3 +37,4 @@ public class WordSearchII {
         return [];
     }
 }
+*/
