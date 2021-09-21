1f1db59a869af67eedd3a7ea7b0818a7cb9c609f
930614591 <930614591@qq.com>
2021-09-21 21:50:41 +0800

    返回链表的中间值链表

--- 14天算法训练/src/Day5/Demo1The_middle_node_of_a_linked_list.java
+++ 14天算法训练/src/Day5/Demo1The_middle_node_of_a_linked_list.java
@@ -7,6 +7,7 @@ public class Demo1The_middle_node_of_a_linked_list {
         ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5)))));
         System.out.println(head.val);
         System.out.println(head.next.next.next.next.next);
+        System.out.println(middleNode(head).val);
     }
 
 
@@ -17,8 +18,20 @@ public class Demo1The_middle_node_of_a_linked_list {
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
-//    public ListNode middleNode(ListNode head) {
-//
-//
-//    }
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow=head,flas=head;
+//        while (slow!=null&&flas!=null){
+//            slow=slow.next;
+//            flas=flas.next.next;
+//        }
+        int len = 0;//存储长度
+        while (slow != null){
+            len++;
+            slow = slow.next;
+        }
+
+        for(int i =len/2;i>0;i--){flas=flas.next;}
+        return flas;
+
+    }
 }
