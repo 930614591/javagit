51ca1686bce584e98c5015c28c0117283876a219
930614591 <930614591@qq.com>
2021-11-11 22:50:45 +0800

    文件IO流

--- Day17/src/inputDemo/Demo1.java
+++ Day17/src/inputDemo/Demo1.java
@@ -7,9 +7,12 @@ import java.io.IOException;
 public class Demo1 {
     public static void main(String[] args) throws IOException {
         //创建一个文件字节输入流对象
+
         FileInputStream fileInputStream=null;
         try {
-             fileInputStream=new FileInputStream("Day17/src/1.txt");
+             fileInputStream=new FileInputStream("Day17/src/1.txt");//本质就是将本地磁盘的文件读取到JVM内存中
+            //官方提供了一些方法 read()一次读取一个字节也就是8位
+            //
             //2.打开文件一个字节一个字节的读
             System.out.println(fileInputStream.read());
             fileInputStream.read();
