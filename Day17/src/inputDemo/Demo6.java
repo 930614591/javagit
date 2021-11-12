c0de0dd7f1c598c7e17fcb0254c465d6e5e2db51
930614591 <930614591@qq.com>
2021-11-13 00:59:14 +0800

    文件IO流

--- Day17/src/inputDemo/Demo6.java
+++ Day17/src/inputDemo/Demo6.java
@@ -7,11 +7,14 @@ import java.io.IOException;
 public class Demo6 {
     public static void main(String[] args) throws IOException {
         //1.使用字符流去写FileWrter
-        FileWriter fileWriter=new FileWriter("Day17/src/1.txt");
-        fileWriter.write("你好哇！");
-        fileWriter.write("\n");
-        fileWriter.write("真的好吗！");
-
+        FileWriter fileWriter=new FileWriter("Day17/src/1.txt",true);
+//        fileWriter.write("你好哇！");//覆盖写法，会抹去原文件
+//        fileWriter.write("\n");
+//        fileWriter.write("真的好吗！");
+//        fileWriter.write("Are you ok!");
+        //
+        fileWriter.append("你好！");
+        fileWriter.append("真不错");
 
         fileWriter.close();
     }
