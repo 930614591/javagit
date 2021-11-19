d1d295f9f486c10c11700b4854a4536016e84bee
930614591 <930614591@qq.com>
2021-11-19 15:35:59 +0800

    //Properties 属性文件类 key=value

--- Day18/src/Demo2/PropertiesDemo2.java
+++ Day18/src/Demo2/PropertiesDemo2.java
@@ -15,7 +15,7 @@ public class PropertiesDemo2 {
         Properties properties=new Properties();
         //2.加载属性文件 读取到properties中
         FileInputStream fileInputStream=new FileInputStream("day18/src/demo2/info.properties");
-        properties.load(fileInputStream);
+        properties.load(fileInputStream);//将文件读取到properties中
         //3.通过键来验证那个数据
         String name= properties.getProperty("name");
         System.out.println(name);
