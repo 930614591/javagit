f33a4d345766b1cb788dc6faa0613d020daa776f
930614591@qq.com <930614591@qq.com>
2022-06-10 23:53:56 +0800

    Changes

--- 训练/Day18/src/Demo2/PropertiesDemo2.java
+++ 训练/Day18/src/Demo2/PropertiesDemo2.java
@@ -14,7 +14,7 @@ public class PropertiesDemo2 {
         //1.创建一个属性文件对象
         Properties properties=new Properties();
         //2.加载属性文件 读取到properties中
-        FileInputStream fileInputStream=new FileInputStream("day18/src/demo2/info.properties");
+        FileInputStream fileInputStream=new FileInputStream("训练/day18/src/demo2/info.properties");
         properties.load(fileInputStream);//将文件读取到properties中
         //3.通过键来验证那个数据
         String name= properties.getProperty("name");
