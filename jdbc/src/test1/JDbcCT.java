595df4dc5d8a618fae43756ebeb7c17110baca3b
930614591@qq.com <930614591@qq.com>
2022-06-21 12:17:54 +0800

    Changes

--- jdbc/src/test1/JDbcCT.java
+++ jdbc/src/test1/JDbcCT.java
@@ -12,8 +12,13 @@ public class JDbcCT {
         Connection conn= DriverManager.getConnection(url,username,password);
 
         //sql
-        String sql="database()";
+        String sql="update sjj set id=10 where id=5";
         //4获取执行sql的对象
         Statement stmt=conn.createStatement();
+        //6.执行sql
+        int count=stmt.executeUpdate(sql);
+        System.out.println(count);
+        stmt.close();
+        conn.close();
     }
 }
