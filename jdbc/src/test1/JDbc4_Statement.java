33bc15bbb67e8c2c96d256f3fadec9c3b8467ce9
930614591@qq.com <930614591@qq.com>
2022-06-21 16:05:09 +0800

    //Statement

---
+++ jdbc/src/test1/JDbc4_Statement.java
@@ -0,0 +1,57 @@
+package test1;
+
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+//Statement
+public class JDbc4_Statement {
+    /*
+    执行dml语句
+     */
+    @Test
+    /*
+    测试方法
+     */
+    public void testDML() throws Exception {
+        //
+        //1、注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        String url="jdbc:mysql://192.168.5.222:3306/test";
+        String username="root";
+        String password="123456";
+        Connection conn= DriverManager.getConnection(url,username,password);
+
+        //sql
+        String sql="update sjj set id=10 where id=5";
+        //4获取执行sql的对象
+        Statement stmt=conn.createStatement();
+        //6.执行sql
+        int count=stmt.executeUpdate(sql);
+        System.out.println(count);
+        stmt.close();
+        conn.close();
+    }
+
+    @Test
+    public void jdbcDDL() throws Exception{
+        //1、注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        String url="jdbc:mysql://192.168.5.222:3306/test";
+        String username="root";
+        String password="123456";
+        Connection conn= DriverManager.getConnection(url,username,password);
+
+        //sql
+        String sql="update sjj set id=10 where id=5";
+        //4获取执行sql的对象
+        Statement stmt=conn.createStatement();
+        //6.执行sql
+        int count=stmt.executeUpdate(sql);
+        System.out.println(count);
+        stmt.close();
+        conn.close();
+    }
+}
--- jdbc/src/test1/JDbcCT.java
+++ jdbc/src/test1/JDbcCT.java
@@ -1,5 +1,4 @@
 package test1;
-
 import java.sql.*;
 
 public class JDbcCT {
@@ -7,8 +6,12 @@ public class JDbcCT {
         //1、注册驱动
         Class.forName("com.mysql.jdbc.Driver");
         String url="jdbc:mysql://192.168.5.222:3306/test";
+        //这里的192.168.5.222:3306指的是我mysql服务器的ip如果是本机的mysql的话用下面这段
+        //String url="jdbc:mysql://127.0.0.1:3306/test";
+        //test指的是所需要用的数据库系统中的具体某一个数据库
         String username="root";
         String password="123456";
+        //数据库的账号密码
         Connection conn= DriverManager.getConnection(url,username,password);
 
         //sql
@@ -17,6 +20,7 @@ public class JDbcCT {
         Statement stmt=conn.createStatement();
         //6.执行sql
         int count=stmt.executeUpdate(sql);
+        //这个方法返回的是数据库受影响的行数
         System.out.println(count);
         stmt.close();
         conn.close();
