98a078d9af329a9744410255dcd1d639dfbd3cb7
930614591@qq.com <930614591@qq.com>
2022-06-22 07:02:34 +0800

    //Statement

--- jdbc/src/JDBC_API/JDbcCT5_ResultSet.java
+++ jdbc/src/JDBC_API/JDbcCT5_ResultSet.java
@@ -2,6 +2,7 @@ package JDBC_API;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class JDbcCT5_ResultSet {
@@ -18,13 +19,21 @@ public class JDbcCT5_ResultSet {
         Connection conn= DriverManager.getConnection(url,username,password);
 
         //sql
-        String sql="update sjj set id=10 where id=5";
+        String sql="select * from sjj";
         //4获取执行sql的对象
         Statement stmt=conn.createStatement();
-        //6.执行sql
-        int count=stmt.executeUpdate(sql);
-        //这个方法返回的是数据库受影响的行数
-        System.out.println(count);
+        //6.执行查询，得到ResultSet
+        ResultSet resultSet=stmt.executeQuery(sql);
+        //处理ResultSet
+        System.out.println("book_id\t|\tbook_name");
+        System.out.println(resultSet);
+        while(resultSet.next()) {
+            int id = resultSet.getInt(1);
+            String name = resultSet.getString("name");
+            System.out.print(id +"\t\t\t");
+            System.out.print(name +"\n");
+
+        }
         stmt.close();
         conn.close();
     }
