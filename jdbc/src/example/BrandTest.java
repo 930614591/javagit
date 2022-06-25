ced158a0de5d32b77666eff0963d5d9a66112b3b
930614591@qq.com <930614591@qq.com>
2022-06-25 21:53:19 +0800

    数据库连接池Drui-完全查询测试

--- jdbc/src/example/BrandTest.java
+++ jdbc/src/example/BrandTest.java
@@ -72,4 +72,105 @@ public class BrandTest {
         preparedStatement.close();
 
     }
+
+    @Test
+    /*添加
+    * 需要参数，除了id之外的所有参数
+    * 结果是布尔类型
+    *
+    * */
+    public void TestSelectADD() throws Exception {
+
+        /*接受页面传递过来的参数*/
+        String brandName="香飘飘";
+        String companyName="香飘飘";
+        int ordered=1;
+        String description="绕地球一圈";
+        int status=1;
+        //1.获取数据库连接connection
+        //3、加载配置文件
+        Properties prop=new Properties();
+        prop.load(new FileInputStream("src/druid.properties"));
+        //4、获取连接池对象
+        DataSource dataSource= DruidDataSourceFactory.createDataSource(prop);
+        //5.获取对象的数据库链接
+        Connection connection = dataSource.getConnection();
+
+        //2.定义sql语句
+        String sql="INSERT INTO `test`.`tb_brand` (`brand_name`, `company_name`, `ordered`, `description`, `status`) VALUES (?, ?, ?, ?, ?);";
+
+        //3.获取pstmt对象
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        //4.设置参数
+        preparedStatement.setString(1,brandName);
+        preparedStatement.setString(2,companyName);
+        preparedStatement.setInt(3,ordered);
+        preparedStatement.setString(4,description);
+        preparedStatement.setInt(5,status);
+        //5.执行sql
+        int i = preparedStatement.executeUpdate();//影响行数
+        //6.处理结果
+        if (i>0){
+            System.out.println("插入陈工");
+        }else {
+            System.out.println("插入失败");
+        }
+        //7.释放资源
+
+        preparedStatement.close();
+        connection.close();
+
+    }
+
+    @Test
+    /*修改
+     * 需要参数，特定的参数必须要id
+     * 结果是布尔类型
+     *
+     * */
+    public void TestSelectupdate() throws Exception {
+
+        /*接受页面传递过来的参数*/
+        String brandName="香飘飘";
+        String companyName="香飘飘";
+        int ordered=1;
+        String description="绕地球一圈";
+        int status=1;
+        //1.获取数据库连接connection
+        //3、加载配置文件
+        Properties prop=new Properties();
+        prop.load(new FileInputStream("src/druid.properties"));
+        //4、获取连接池对象
+        DataSource dataSource= DruidDataSourceFactory.createDataSource(prop);
+        //5.获取对象的数据库链接
+        Connection connection = dataSource.getConnection();
+
+        //2.定义sql语句
+        String sql="update tb_brand set brand_name=?,company_name=?,ordered=?,description=?,status=? where id=?;";
+
+        //3.获取pstmt对象
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        //4.设置参数
+        preparedStatement.setString(1,brandName);
+        preparedStatement.setString(2,companyName);
+        preparedStatement.setInt(3,ordered);
+        preparedStatement.setString(4,description);
+        preparedStatement.setInt(5,status);
+        preparedStatement.setInt(6,2);
+        //5.执行sql
+        int i = preparedStatement.executeUpdate();//影响行数
+        //6.处理结果
+        if (i>0){
+            System.out.println("插入陈工");
+        }else {
+            System.out.println("插入失败");
+        }
+        //7.释放资源
+
+        preparedStatement.close();
+        connection.close();
+
+    }
 }
