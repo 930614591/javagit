74108428200b47982ffbc023e943df3959716405
Your Name <930614591@qq.com>
2022-09-12 09:44:34 +0800

    SpringMVC 基础demo以及环境加载，扫描排除

---
+++ SpringMvc/demo1/src/main/java/com/ang/config/ServletConfig.java
@@ -0,0 +1,45 @@
+package com.ang.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+    //定义一个servlet容器启动配置类，加载spring的配置
+//    public class ServletConfig extends AbstractDispatcherServletInitializer {
+//        //加载springMVC容器配置
+//        @Override
+//        protected WebApplicationContext createServletApplicationContext() {
+//            AnnotationConfigWebApplicationContext web=new AnnotationConfigWebApplicationContext();
+//            web.register(SpringMvcConfig.class);
+//            return web;
+//        }
+//        //设置那些请求归属SpringMVC处理
+//        @Override
+//        protected String[] getServletMappings() {
+//            return new String[]{"/"};
+//        }
+//    //加载Spring容器配置
+//        @Override
+//        protected WebApplicationContext createRootApplicationContext() {
+//            AnnotationConfigWebApplicationContext ctx=new AnnotationConfigWebApplicationContext();
+////            web.register(SpringConfig.class);
+////            return ctx;
+//        }
+//    }
+    public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+        @Override
+        protected Class<?>[] getRootConfigClasses() {
+            return new Class[]{SpringMvcConfig.class};
+        }
+
+        @Override
+        protected Class<?>[] getServletConfigClasses() {
+            return new Class[]{SpringConfig.class};
+        }
+
+        @Override
+        protected String[] getServletMappings() {
+            return new String[]{"/"};
+        }
+    }
---
+++ SpringMvc/demo1/src/main/java/com/ang/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.ang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//定义@Controller
+//2.1使用Controller定义bean
+@Controller
+public class UserController {
+    //2.2设定访问路径
+    //2.3设置当前操作的返回值类型
+    @RequestMapping("/save")
+    @ResponseBody
+    public String save(){
+        System.out.println("save");
+        return "{'module':'springmvc'}";
+    }
+
+    @RequestMapping("/delete")
+    @ResponseBody
+    public String delete(){
+        System.out.println("delete");
+        return "{'module':'springmvc delete'}";
+    }
+}
